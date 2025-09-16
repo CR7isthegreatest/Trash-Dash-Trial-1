87377f316d9b9ba50915321feafecb5dd274aea5
CR7isthegreatest <aarbens@chbc.school.nz>
2025-09-16 15:03:00 +1200

    stuff

--- Assets/SCRIPTS/EndLevel.cs
+++ Assets/SCRIPTS/EndLevel.cs
@@ -2,13 +2,27 @@ using UnityEngine;
 
 public class EndLevel : MonoBehaviour
 {
+    [SerializeField] int requiredPlasticBottles = 0;
     public GameManager gameManager;
+    PlayerInventory playerInventory;
+
+    private void Awake()
+    {
+        playerInventory = FindFirstObjectByType<PlayerInventory>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            gameManager.CompleteLevel();
+            if (playerInventory.NumberOfDiamonds < requiredPlasticBottles)
+            {
+                gameManager.EndGame();
+            }
+            else
+            {
+                gameManager.CompleteLevel();
+            }
         }
         
     }
--- Assets/SCRIPTS/LevelComplete.cs
+++ Assets/SCRIPTS/LevelComplete.cs
@@ -6,6 +6,10 @@ public class LevelComplete : MonoBehaviour
 {
     public void LoadNextLevel ()
     {
+        if (SceneManager.GetActiveScene().buildIndex == 3)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
